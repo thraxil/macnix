91e2f52461e7a128909a64f041a998abba6e6cfb
Anders Pearson <anders@thraxil.org>
2023-10-18 14:56:24 +0100

    some system settings

--- modules/system.nix
+++ modules/system.nix
@@ -7,9 +7,38 @@
     defaults = {
       menuExtraClock.Show24Hour = true; # show 24 hour clock
 
-      # other macOS's defaults configuration.
-      # ......
+      NSGlobalDomain = {
+        "com.apple.sound.beep.feedback" = 0;
+      };
+      CustomUserPreferences = {
+        NSGlobalDomain = {
+	  WebKitDeveloperExtras = true;
+        };
+	"com.apple.desktopservices" = {
+          # Avoid creating .DS_Store files on network or USB volumes
+          DSDontWriteNetworkStores = true;
+          DSDontWriteUSBStores = true;
+        };
+	"com.apple.screensaver" = {
+          # Require password immediately after sleep or screen saver begins
+          askForPassword = 1;
+          askForPasswordDelay = 0;
+        };
+	"com.apple.screencapture" = {
+          location = "~/Desktop";
+          type = "png";
+        };
+	"com.apple.AdLib" = {
+          allowApplePersonalizedAdvertising = false;
+        };
+	"com.apple.ImageCapture".disableHotPlug = true;
+      };
     };
+  };
+
+  keyboard = {
+    enableKeyMapping = true;
+    remapCapsLockToControl = false;
   };
 
   # Add ability to used TouchID for sudo authentication
